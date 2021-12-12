66ceb13f006f0fc661b24151cecc31ff1549bcad
Rumi Aoyagi <ru25ruth@gmail.com>
2021-12-12 18:59:14 +0900

    docs: add cmt about file in bikeshare

--- bikeshare.py
+++ bikeshare.py
@@ -1,7 +1,7 @@
 import time
 import pandas as pd
 import numpy as np
-
+# This file is also used in Github Project
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
